8f62e05afeedb63db5d6bc8d848e076264282327
Arsam Mohammadi <Arsam2001mohammadi@gmail.com>
2023-04-10 20:45:19 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <header className="header">
+            <img src='https://ui-themez.smartinnovates.net/items/zain/img/logo-light.png'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+    return <div className='content'>
+        <div>
+          <img src='https://images.skyscrapercenter.com/building/bmw-building_anton-schedlbauer-munich1.jpg'/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+
+      </div>
+}
+
+export default Profile;
